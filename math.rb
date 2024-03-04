8b42e57c1f18d0435bcb3ab9819f7cd3a652ea5e
Eric <ericbradshaw98@gmail.com>
2024-03-04 12:15:29 -0500

    added a few starter files

---
+++ math.rb
@@ -0,0 +1,7 @@
+#Read the description above again and extract / write down the nouns that you feel could make for important entities (manifested in the form of classes) to contain (encapsulate) logic as part of this app.
+
+#P1 P2 score, answers, lives
+
+#What information is relevant to each class?
+#What will they need in order to be initialized?
+#What public methods will be defined on them?
