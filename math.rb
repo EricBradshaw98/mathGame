7ea2d8b724696bdc2920ece42046d2bc83d98097
Eric <ericbradshaw98@gmail.com>
2024-03-04 12:39:26 -0500

    initialized both players in game

--- math.rb
+++ math.rb
@@ -5,3 +5,18 @@
 #What information is relevant to each class?
 #What will they need in order to be initialized?
 #What public methods will be defined on them?
+
+#require the two classes
+require_relative 'playerclass'
+require_relative 'questionclass'
+
+#tested running question in here, works.
+#initialize game class
+class Game
+  def initialize
+    @player1 = PlayerInfo.new('Player 1')
+    @player2 = PlayerInfo.new('Player 2')
+    @current_player = @player1
+  end
+
+  
